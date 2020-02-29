b7fa1f31735f6732b3ecb42cdedcbbdc219a4765
Pedro <pedrohba18@gmail.com>
2020-02-29 16:42:47 -0300

    requisição de produtos da api, readme com info sobre execução da api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,91 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
+import {formatPrice} from '../../utils/format';
+import api from '../../services/api';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
-          alt=""
-        />
-        <strong> Tênis legal</strong>
-        <span>R$29, 90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-            {' '}
-            3
-          </div>
-          <span>aidiconar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
-          alt=""
-        />
-        <strong> Tênis legal</strong>
-        <span>R$29, 90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-            {' '}
-            3
-          </div>
-          <span>aidiconar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
-          alt=""
-        />
-        <strong> Tênis legal</strong>
-        <span>R$29, 90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-            {' '}
-            3
-          </div>
-          <span>aidiconar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
-          alt=""
-        />
-        <strong> Tênis legal</strong>
-        <span>R$29, 90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-            {' '}
-            3
-          </div>
-          <span>aidiconar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom2.jpg?ims=326x"
-          alt=""
-        />
-        <strong> Tênis legal</strong>
-        <span>R$29, 90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-            {' '}
-            3
-          </div>
-          <span>aidiconar ao carrinho</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+
+class Home extends Component {
+    state = {
+         products:[],
+    };
+
+    async componentDidMount(){
+        const res = await api.get('products');
+        const data = res.data.map(product => ({
+            ...product,
+            //para o render não ficar atualizando múltiplas vezes, eu já coloco direto o preço formatado no objeto do produto.
+            priceFormatted: formatPrice( product.price),
+        }))
+
+
+        this.setState({products: data});
+
+    }
+
+
+    render() {
+        const {products} = this.state;
+        return (
+          <ProductList>
+              {products.map(product =>(
+                <li key = {product.id}>
+                <img
+                src = {product.image}
+                alt={product.title}
+                />
+                <strong>{product.title}</strong>
+              <span>{product.priceFormatted}</span>
+                <button type="button">
+                <div>
+                    <MdAddShoppingCart size={16} color="#fff" />
+                    {' '}
+                    3
+                </div>
+                <span>adicionar ao carrinho</span>
+                </button>
+                </li>
+              ))}
+          </ProductList>
+        );
+    }
 }
+
+export default Home;
+
